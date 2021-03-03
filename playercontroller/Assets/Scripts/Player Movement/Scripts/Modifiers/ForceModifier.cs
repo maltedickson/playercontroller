7229fe5e50a186b3e95187ad69051d087c366367
PatAPizza56 <63616729+PatAPizza56@users.noreply.github.com>
2021-03-03 14:14:27 -0500

    Updated force modifier
    Updated force modifier

--- playercontroller/Assets/Scripts/Player Movement/Scripts/Modifiers/ForceModifier.cs
+++ playercontroller/Assets/Scripts/Player Movement/Scripts/Modifiers/ForceModifier.cs
@@ -3,25 +3,17 @@ using UnityEngine;
 public class ForceModifier : MonoBehaviour, IMovementModifier
 {
     Vector3 currentForce = Vector3.zero;
-    Vector3 currentRawForce = Vector3.zero;
 
     public Vector3 Modify(ModifierInfo info, PlayerMoverConfig config)
     {
-        currentForce = Vector3.Lerp(currentForce, Vector3.zero, 5 * Time.deltaTime);
-        if (currentForce.magnitude > config.maxForce) currentForce = currentForce.normalized * config.maxForce;
+        Vector3 currentForce = this.currentForce;
+        this.currentForce = Vector3.zero;
 
-        Vector3 currentRawForce = this.currentRawForce;
-        this.currentRawForce = Vector3.zero;
-
-        return currentForce + currentRawForce;
+        return currentForce;
     }
 
     public void AddForce(Vector3 force)
     {
         currentForce += force;
-    }
-    public void AddRawForce(Vector3 force)
-    {
-        currentRawForce += force;
     }
 }
