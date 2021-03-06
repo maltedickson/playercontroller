34778fe416b4f9d39e14a1a351ab204238cbcc6a
maltedickson <malte.dickson@gmail.com>
2021-03-06 10:40:45 +0100

    Added attack interval to rocket launcher

---
+++ playercontroller/Assets/Rocket Launcher/Scripts/RocketLauncher.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RocketLauncher : MonoBehaviour
+{
+    [SerializeField] GameObject rocketPrefab = null;
+    [SerializeField] float rocketSpeed = 18f;
+    [SerializeField] float attackInterval = 0.8f;
+    bool isShooting = false;
+    [Space]
+    [SerializeField] InputAction shootDown;
+    [SerializeField] InputAction shootUp;
+    bool wantsToShoot = false;
+
+    void OnEnable()
+    {
+        shootDown.Enable();
+        shootUp.Enable();
+    }
+
+    void OnDisable()
+    {
+        shootDown.Disable();
+        shootUp.Disable();
+    }
+
+    void Start()
+    {
+        shootDown.performed += _ => wantsToShoot = true;
+        shootUp.performed += _ => wantsToShoot = false;
+    }
+
+    void Update()
+    {
+        Debug.Log(wantsToShoot);
+
+        if (wantsToShoot && !isShooting) StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        Debug.Log("shoot");
+        isShooting = true;
+        Rocket rocket = Instantiate(rocketPrefab, transform.position, transform.rotation).GetComponent<Rocket>();
+        rocket.SetSpeed(rocketSpeed);
+        yield return new WaitForSeconds(attackInterval);
+        isShooting = false;
+    }
+}
